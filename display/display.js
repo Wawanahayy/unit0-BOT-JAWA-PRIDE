42fee87299349010b5f68333d198ae9042bb6ca1
Wawanahayy <98287702+Wawanahayy@users.noreply.github.com>
2024-06-25 00:36:38 +0700

    Update display.js

--- display/display.js
+++ display/display.js
@@ -2,13 +2,13 @@ const colors = require('colors');
 
 function displayHeader() {
   process.stdout.write('\x1Bc');
-  console.log(colors.cyan('========================================================='));
-  console.log(colors.cyan('========================================================='));
-  console.log(colors.cyan('================ unit0-BOT-JAWA-PRIDE  =================='));
-  console.log(colors.cyan('===========     Created by JAWA-PRIDE  =================='));
-  console.log(colors.cyan('=========== https://t.me/AirdropJP_JawaPride ============'));
-  console.log(colors.cyan('========================================================='));
-  console.log(colors.cyan('========================================================='));
+  console.log(colors.bgGreen('========================================================='));
+  console.log(colors.bgGreen('========================================================='));
+  console.log(colors.bgWhite('===================== unit0-BOT ========================='));
+  console.log(colors.bgWhite('============== modify all by JAWA-PRIDE  ================'));
+  console.log(colors.bgWhite('=========== https://t.me/AirdropJP_JawaPride ============'));
+  console.log(colors.bgGreen('========================================================='));
+  console.log(colors.bgGreen('========================================================='));
   console.log();
 }
 
