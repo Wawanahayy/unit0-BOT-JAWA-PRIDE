c01b5bad9cf2675b73aab7469d017ca808675926
Wawanahayy <98287702+Wawanahayy@users.noreply.github.com>
2024-06-24 20:52:54 +0700

    Update sleep.js

--- src/sleep.js
+++ src/sleep.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 
 const sleep = (ms) => {
@@ -8,32 +6,5 @@ const sleep = (ms) => {
     });
 };
 
-const writeFile = (fileName, programName) => {
-    // do work
-    const today = new Date();
-    const fileLine = `\n\nhello from the ${programName} program ${today.toLocaleDateString()} ${today.toLocaleTimeString()}`;
-    fs.appendFileSync(fileName, fileLine, (err) => {
-        if (err) {
-            console.error(err);
-        }
-    });
-};
 
-const outputFile = (fileName) => {
-    // originally copied from https://nodejs.dev/en/learn/reading-files-with-nodejs
-    // write out file contents to screen
-    console.log('\n\n final contents of file');
-    try {
-        const fileContents = fs.readFileSync(fileName, 'utf8');
-        console.log(fileContents);
-    } catch (error) {
-        console.error(error);
-    }
-};
 
-const exampleFile = 'files/HelloWorld.txt';
-
-module.exports.sleep = sleep;
-module.exports.exampleFile = exampleFile;
-module.exports.writeFile = writeFile;
-module.exports.outputFile = outputFile;
